8f16c44e2fcf337ab4be1416d82951c65cd7dbd5
Phillip Eckstein <sirtario9@gmail.com>
2021-04-12 11:52:32 +0200

    Setup Testing

---
+++ test/Vehicletests.java
@@ -0,0 +1,14 @@
+package vehicleBooking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Vehicletests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
