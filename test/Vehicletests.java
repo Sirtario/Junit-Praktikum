9e968a58ae898c7ec318de08790995d52a009c96
Phillip Eckstein <sirtario9@gmail.com>
2021-04-15 11:38:09 +0200

    Add Testcase to test if unavailable vehicles can be booked

--- test/Vehicletests.java
+++ test/Vehicletests.java
@@ -1,14 +1,24 @@
-package vehicleBooking;
+
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
+
+import vehicleBooking.*;
 
 class Vehicletests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void TestbookingUnavailableVehicle() {
+		VehicleManagement management = new VehicleManagement();
+		SUV vehicle = new SUV("x3", 100);
+		Customer costumer = new Customer(12,"Henry");
+		
+		management.addVehicle(vehicle);
+		
+		vehicle.setAvailable(false);
+		assertFalse(vehicle.isAvailable());
+		
+		assertFalse(management.bookVehicle(vehicle,costumer));
 	}
 
 }
